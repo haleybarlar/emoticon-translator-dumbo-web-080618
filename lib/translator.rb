b8b5cc22d9e7d4711f12bb7c472eee946dc596c1
Haley Barlar <hbarlar1@gmail.com>
2018-08-05 20:22:07 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ def load_library(file_path)
   path = YAML.load_file(file_path)
   file_path.each do | us, japan |
     hash = {"get_meaning" => {[japan] => [us]}, "get_emoticon" => {[us] => [japan]}}
-  
+  end
   return hash
 end
 
