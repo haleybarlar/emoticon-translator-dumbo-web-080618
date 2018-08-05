9b42146f2fbcb0d09d21760f59f62590536d225f
Haley Barlar <hbarlar1@gmail.com>
2018-08-05 20:54:34 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,6 +20,7 @@ def get_japanese_emoticon (file_path, emoticon)
   if hash != file_path
     return "Sorry, that emoticon was not found"
   end
+  result
 end
 
 def get_english_meaning(file_path, emoticon)
