dbc908bbb92fb2ed35601f367d9d42c641ce08cb
Haley Barlar <hbarlar1@gmail.com>
2018-08-05 20:30:56 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,8 +8,9 @@ def load_library(file_path)
   return hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon (file_path, emoticon)
+  load_library(file_path)
+  
 end
 
 def get_english_meaning
