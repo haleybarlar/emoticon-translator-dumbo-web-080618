318ae2399f44c1d7adfb583b2f8a9fa061ceac3b
Haley Barlar <hbarlar1@gmail.com>
2018-08-05 20:44:58 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "yaml"
 def load_library(file_path)
   path = YAML.load_file(file_path)
   path.each do |english, emoticons|
-      hash = {"get_meaning" => {emoticons.last => english}, "get_emoticon" => {}}
+      hash = {"get_meaning" => {emoticons.last => english}, "get_emoticon" => {emoticons.first => emoticons.last}}
 
   return hash
 end
